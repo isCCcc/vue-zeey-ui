5aea0b51416bb7ffff7a6a7c4d157fb1edb9a105
zixi <zixi@beansmile.com>
2024-11-01 11:30:47 +0800

    fix: export file

--- packages/index.ts
+++ packages/index.ts
@@ -1,32 +1,17 @@
 //index.ts
-import type { App } from 'vue'
-import ZButton from './Button/index'
+import type { App } from 'vue';
+import ZButton from './Button/index';
 
-// 所有组件列表
-const components = [
-    ZButton,
-]
+// 组件列表
+const components = [ZButton];
 
-// 定义 install 方法
-const install = (app: App): void => {
-  // 遍历注册所有组件
-  /*
-    component.__name ts报错
-    Argument of type 'string | undefined' is not assignable to parameter of type 'string'.
+export { ZButton };
 
-    Type 'undefined' is not assignable to type 'string'.ts(2345)
-    解决方式一：使用// @ts-ignore
-    解决方式二：使用类型断言 尖括号语法(component.__name) 或 as语法(component.__name as string)
-  */
-  components.forEach(component => app.component(component.__name as string, component))
-}
-
-export {
-    ZButton,
-}
-
-const VueZeeyUI = {
-  install
-}
-
-export default VueZeeyUI
+export default {
+  // 定义 install 方法
+  install(app: App) {
+    components.forEach((component) =>
+      app.component(component.__name as string, component),
+    );
+  },
+};
